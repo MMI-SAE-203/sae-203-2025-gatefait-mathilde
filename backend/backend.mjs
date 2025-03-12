2fc4812e2081006097fc1c24be12ba16d21b8a91
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2025-03-12 13:57:53 +0100

    card button,intro index, route dynamique

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -6,41 +6,59 @@ export default pb;
 
 
 // films trié par date de projection
+
 export async function allFilms() {
-const records = await pb.collection('films').getFullList({ sort: 'date_projection' }); ;
-return records ;
+  const records = await pb.collection('films').getFullList({ sort: 'date_projection' });
+    let films = records.map(film => {
+    film.imgUrl = pb.files.getURL(film, film.image);
+    return film; 
+  });
+  return films; 
 }
+
 
 //liste activités par date de projections
 export async function allActivites() {
-const records = await pb.collection('activites').getFullList({ sort: 'date_et_heure' }); ;
+const records = await pb.collection('activites').getFullList({ sort: 'date_et_heure' }); 
+let activites = records.map(activite => {
+    activite.imgUrl = pb.files.getURL(activite, activite.image);
+    return activite; 
+  });
 return records ;
 }
 
 //invites par ordre alphabetique
-
 export async function allInvitesByAgentName() {
-const records = await pb.collection('invites').getFullList({ sort: 'nom' }); 
-return records;
+  const records = await pb.collection('invites').getFullList({ sort: 'nom' });
+    let invites = records.map(invite => {
+    invite.imgUrl = pb.files.getURL(invite, invite.image);
+    return invite; 
+  });
+  return invites; 
 }
+
 
 //film par son id
 export async function OneID(id) {
-const oneRecords = await pb.collection('films').getOne(id) ;
-return oneRecords ;
+    let film = await pb.collection('films').getOne(id) ;
+    film.imageUrl = pb.files.getURL(film, film.image);
+    return film; 
 }
 
 //activite par son id
 export async function OneIDAct(id) {
-const oneRecords = await pb.collection('activites').getOne(id) ;
-return oneRecords ;
+    let activite = await pb.collection('activites').getOne(id) ;
+    activite.imageUrl = pb.files.getURL(activite, activite.image);
+    return invite; 
 }
 
 //invite par son id
 export async function OneIDInv(id) {
-const oneRecords = await pb.collection('invites').getOne(id) ;
-return oneRecords ;
+    let invite = await pb.collection('invites').getOne(id) ;
+    invite.imageUrl = pb.files.getURL(invite, invite.image);
+    return invite; 
 }
+
 
 //act d'un invite par son id
 export async function ActInvitesById(id) {
