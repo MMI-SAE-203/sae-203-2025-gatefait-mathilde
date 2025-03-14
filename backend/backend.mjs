4e3acfec7f307c9096f132f0c6a2e39db7c953ce
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2025-03-14 09:32:56 +0100

    Update backend.mjs

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -2,7 +2,7 @@
 
 
 import PocketBase from 'pocketbase';
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://festivhalloween.mathilde-gatefait.fr:443');
 export default pb;
 
 
